e42f515d07f66048bf8e87a7c73bd7540895e249
Godefroid Chapelle <gotcha@bubblenet.be>
2013-06-11 11:39:14 +0200

    tune setup.py

--- setup.py
+++ setup.py
@@ -12,14 +12,15 @@ setup(name='collective.whoiswho',
       version=version,
       description="Display members and groups",
       long_description=long_description,
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Programming Language :: Python",
-        ],
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.6",
+          "Framework :: Plone",
+          "Framework :: Plone :: 4.2",
+      ],
       keywords='',
-      author='',
-      author_email='',
+      author='Godefroid Chapelle',
+      author_email='gotcha@bubblenet.be',
       url='http://svn.plone.org/svn/collective/',
       license='gpl',
       packages=find_packages('src'),
@@ -29,6 +30,7 @@ setup(name='collective.whoiswho',
       zip_safe=False,
       install_requires=[
           'setuptools',
+          'Plone'
           # -*- Extra requirements: -*-
       ],
       entry_points="""
